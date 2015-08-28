4b0c7547b301ead4215f5da8e127085cc43d5a34
Anna Karingal <anna.karingal@gmail.com>
2015-08-28 17:25:23 -0400

    Change polls/answered url to polls/users url

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^all/$', views.AllView.as_view(), name='all'),
-    url(r'^answered/$', views.AnsweredView.as_view(), name='answered'),
+    url(r'^users/$', views.UserIndexView.as_view(), name='users'),
     url(r'^new/$', views.new, name='new'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
