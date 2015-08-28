b4f06f9388396332dd30817baa01d5aa7fbb9b67
Anna Karingal <anna.karingal@gmail.com>
2015-08-28 12:56:03 -0400

    Create poll model form and choice inline formset

---
+++ polls/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from django.forms.models import inlineformset_factory
+
+from polls.models import Poll, Choice
+
+ChoiceFormSet = inlineformset_factory(Poll, Choice, can_delete=False, extra=5)
+
+class PollForm(ModelForm):
+    class Meta:
+        model = Poll
+        fields = ['question',]
